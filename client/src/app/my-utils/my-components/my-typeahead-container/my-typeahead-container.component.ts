97434f8aa15daddb4d8242dbf4d90c395b8ce734
younes-bh <benhoumich.younes@gmail.com>
2017-10-04 18:21:31 +0200

    imporve Typeahead

--- client/src/app/my-utils/my-components/my-typeahead-container/my-typeahead-container.component.ts
+++ client/src/app/my-utils/my-components/my-typeahead-container/my-typeahead-container.component.ts
@@ -15,8 +15,8 @@ export class MyTypeaheadContainerComponent implements OnInit {
   @Output() selectedItem: EventEmitter<string> = new EventEmitter<string>();
   private _query: string;
   filteredList: Array<string> = new Array<string>();
-  activeItem: string;
-  // @ViewChild('ul') ulList: ElementRef;
+  activeDim: ElementRef;
+  @ViewChild('divList') divList: ElementRef;
 
   constructor() {}
 
@@ -39,59 +39,68 @@ export class MyTypeaheadContainerComponent implements OnInit {
   }
 
   select() {
-    if (this.activeItem) {
-      this.selectedItem.emit(this.activeItem);
+    if (this.activeDim) {
+      this.selectedItem.emit(this.activeDim.nativeElement.textContent.trim());
       this.hide();
     }
   }
 
-  setActiveItem(item: string) {
-    this.activeItem = item;
+  setActiveItem(event: any) {
+    // console.log(event.target.textContent.trim());
+    if (this.activeDim){
+      this.activeDim.nativeElement.removeClass('active');
+    }
+    this.activeDim = event.target as ElementRef;
+    console.log(event.target);
+    console.log(this.activeDim);
+    this.activeDim.nativeElement.addClass('active');
   }
 
-  activateNextItem(){
-    const currentIndex = this.filteredList.indexOf(this.activeItem);
-    if (currentIndex >= 0 && currentIndex <= (this.filteredList.length - 2)) {
-      this.activeItem = this.filteredList[currentIndex + 1];
-    } else {
-      this.activeItem = this.filteredList[0];
-    }
+  activateNextItem() {
+    this.activeDim = this.activeDim.nativeElement.nextSibling();
+    // const currentIndex = this.filteredList.indexOf(this.activeItem);
+    // if (currentIndex >= 0 && currentIndex <= (this.filteredList.length - 2)) {
+    //   this.activeItem = this.filteredList[currentIndex + 1];
+    // } else {
+    //   this.activeItem = this.filteredList[0];
+    // }
   }
 
-  activatePreviousItem(){
-    const currentIndex = this.filteredList.indexOf(this.activeItem);
-    if (currentIndex >= 1 && currentIndex <= (this.filteredList.length - 1)) {
-      this.activeItem = this.filteredList[currentIndex - 1];
-    } else {
-      this.activeItem = this.filteredList[this.filteredList.length - 1];
-    }
+  activatePreviousItem() {
+    this.activeDim = this.activeDim.nativeElement.previousSibling();
+    // const currentIndex = this.filteredList.indexOf(this.activeItem);
+    // if (currentIndex >= 1 && currentIndex <= (this.filteredList.length - 1)) {
+    //   this.activeItem = this.filteredList[currentIndex - 1];
+    // } else {
+    //   this.activeItem = this.filteredList[this.filteredList.length - 1];
+    // }
   }
 
   hide() {
-    this.activeItem = null;
+    this.activeDim = null;
     this.filteredList = [];
   }
 
-  scroll() {
-    // console.log(container.nativeElement);
-    //(item === activeItem) ? (container.offsetTop - 50) : 10
-    // function updateListSelection(liID) {
-    //
-    //   var list = document.getElementById("id Tag Of The <UL> element"),
-    //     targetLi = document.getElementById(liID); // id tag of the <li> element
-    //
-    //   list.scrollTop = (targetLi.offsetTop - 50);
-    // };
-    // const delta = this.ulList.nativeElement.getElementsByTagName('li')[0].clientHeight;
-    // console.log( delta);
-    // this.ulList.nativeElement.scrollTop = this.ulList.nativeElement.scrollTop  this.ulList.nativeElement.clientHeight / 2 - delta;
-    // //this.ulList.nativeElement.getElementsByClassName('active')[0].scrollIntoView();
-
-
-    //this.ulList.nativeElement.scrollTop = this.ulList.nativeElement.scrollTop + 20;
-    //console.log(this.ulList.nativeElement.getElementsByClassName('active')[0]);
-    //this.elementRef.nativeElement.scrollIntoView(50);
-
-  }
+  // scroll() {
+  //   // console.log(container.nativeElement);
+  //   //(item === activeItem) ? (container.offsetTop - 50) : 10
+  //   // function updateListSelection(liID) {
+  //   //
+  //   //   var list = document.getElementById("id Tag Of The <UL> element"),
+  //   //     targetLi = document.getElementById(liID); // id tag of the <li> element
+  //   //
+  //   //   list.scrollTop = (targetLi.offsetTop - 50);
+  //   // };
+  //   // const delta = this.ulList.nativeElement.getElementsByTagName('li')[0].clientHeight;
+  //   // console.log( delta);
+  //   // this.ulList.nativeElement.scrollTop = this.ulList.nativeElement.scrollTop  this.ulList.nativeElement.clientHeight / 2 - delta;
+  //   // //this.ulList.nativeElement.getElementsByClassName('active')[0].scrollIntoView();
+  //
+  //
+  //   //this.ulList.nativeElement.scrollTop = this.ulList.nativeElement.scrollTop + 20;
+  //   //console.log(this.ulList.nativeElement.getElementsByClassName('active')[0]);
+  //   //this.elementRef.nativeElement.scrollIntoView(50);
+  //
+  // }
 
 }
