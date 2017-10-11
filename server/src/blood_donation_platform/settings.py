cb1bb582c07f74166eca63c6e16ae4dca1c3008a
younes-bh <benhoumich.younes@gmail.com>
2017-10-11 23:13:28 +0200

    Change pagination to  'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',

--- server/src/blood_donation_platform/settings.py
+++ server/src/blood_donation_platform/settings.py
@@ -152,7 +152,7 @@ REST_FRAMEWORK = {
     ),
 
     #Pagination Settings
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 
     'DEFAULT_PARSER_CLASSES': (
