0c5b011296b621974d64d785c39428269a05deeb
Younes <younes@MacBook-Pro-de-Younes.local>
2017-10-22 21:51:00 +0200

    add some spaces

--- client/src/app/donations/index.ts
+++ client/src/app/donations/index.ts
@@ -1,11 +1,12 @@
 export * from './donation-create/donation-create.component';
 export * from './donation-detail/donation-detail.component';
 export * from './donation-list/donation-list.component';
+export * from './donation-search-filter/donation-search-filter.component';
 export * from './donation-list-all/donation-list-all.component';
 export * from './donation-list-my/donation-list-my.component';
 export * from './donation-thumbnail/donation-thumbnail.component';
 export * from './donation-update/donation-update.component';
 export * from './donation.component';
 export * from './donation.routes';
-export * from './donation-search-filter/donation-search-filter.component';
+
 
