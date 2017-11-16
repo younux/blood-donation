848aea9ac81c53cfc4e1158a2d98d0e1d068ea00
younes-bh <benhoumich.younes@gmail.com>
2017-11-16 20:44:58 +0100

    small fix

--- client/src/app/profiles/profile-my/profile-my.component.ts
+++ client/src/app/profiles/profile-my/profile-my.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Profile} from "../../shared/models/profile.model";
 import {AuthenticationService} from "../../shared/services/authentication.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-profile-my',
@@ -12,10 +13,12 @@ export class ProfileMyComponent implements OnInit {
   myProfile: Profile;
   tab = 'information';
 
-  constructor(private authenticationService: AuthenticationService) { }
+  constructor(private authenticationService: AuthenticationService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.myProfile = this.authenticationService.profile;
+    this.tab = this.route.snapshot.queryParams['tab'] || this.tab;
   }
 
 
